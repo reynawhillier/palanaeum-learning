22779e0914eb1ef85e8acad49cfb34961b5757e6
salmaragehh <156117113+salmaragehh@users.noreply.github.com>
2026-07-12 19:59:13 -0400

    Created fake data on front end to test routing

--- start/routes.ts
+++ start/routes.ts
@@ -11,8 +11,12 @@ import { middleware } from '#start/kernel'
 import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 //import AssignmentsController from '#controllers/assignments_controller'
+import { fakeUsers } from './fakeData.js'
 
-//router.on('/').render('pages/home').as('home')
+
+router.get('/', async ({ view }) => {
+  return view.render('pages/home')
+})
 
 router
   .group(() => {
@@ -36,40 +40,62 @@ router.get('/profile', async ({ view }) => {
   return view.render('pages/profile')
 })
 
-router.get('/', async ({ view }) => {
-  return view.render('pages/dashboards/student')
+router.get('/student/dashboard', async ({ view }) => {
+  return view.render('pages/dashboards/student', {
+    user: fakeUsers.student,
+    courses: fakeUsers.student.courses
+  })
 })
 
-router.get('/admin', async ({ view }) => {
-  return view.render('pages/dashboards/admin')
+router.get('/professor/dashboard', async ({ view }) => {
+  return view.render('pages/dashboards/professor', {
+    user: fakeUsers.professor,
+    courses: fakeUsers.professor.courses
+  })
 })
 
-router.get('/professor', async ({ view }) => {
-  return view.render('pages/dashboards/professor')
+router.get('/admin/dashboard', async ({ view }) => {
+  return view.render('pages/dashboards/admin', {
+    user: fakeUsers.admin
+  })
 })
 
 router.get('/courses/:id', async ({ view, params }) => {
+  const courseId = Number(params.id)
+  const user = fakeUsers.professor
+
+  const course = user.courses.find(
+    course => course.id === courseId
+  )
+
   return view.render('pages/courses/dashboard', {
-    courseId: params.id
+    user,
+    course
   })
 })
 
 router.get('/courses/:id/assignments', async ({ view, params }) => {
+    const user = fakeUsers.professor
 
-    const role = 'professor' // Replace later when db is connected 
+    if(user.role === 'professor') {
 
-    if (role === 'professor') {
-        return view.render('pages/courses/assignments/professor', {
-            courseId: params.id
-        })
+        return view.render(
+            'pages/courses/assignments/professor',
+            {
+                user,
+                courseId: params.id
+            }
+        )
+
     }
 
-    else {
-        return view.render('pages/courses/assignments/student', {
+    return view.render(
+        'pages/courses/assignments/student',
+        {
+            user,
             courseId: params.id
-        })
-    }
-
+        }
+    )
 })
 
 router.get('/courses/:id/assignments/create', async ({ view, params }) => {
