e7c76e730905860f7e6382348eb29cd5aaf87630
reynawhillier <reyna@whillier.com>
2026-07-17 17:13:31 -0400

    Minor Error Changes
    Minor error change with assignment-controller, moved a reminder comment I misplaced in the original setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,6 @@ npm install
 
 node ace migration:run
 
-# use await Table.firstOrCreate (
-#     { primaryKey: keyinformation },
-#     {   other attribute : value,
-#         other attribute : value
-#     }
-# )
 node ace db:seed
 
 npm run dev
